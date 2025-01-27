a7fd4713864a741b97d3d32d395d643e7e2571d2
Alon A. Rabinowitz <45129051+AlonRR@users.noreply.github.com>
2025-01-27 12:21:02 +0200

    Added flame sensor pin definitions and readings to main loop.

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,20 @@
 #include <Arduino.h>
 
+#define FLAME_SENSOR_ANALOG_PIN 32
+#define FLAME_SENSOR_DIGITAL_PIN 34
+
 void setup()
 {
   Serial.begin(115200);
+  pinMode(FLAME_SENSOR_DIGITAL_PIN, INPUT);
+  pinMode(FLAME_SENSOR_ANALOG_PIN, INPUT);
 }
 
 void loop()
 {
-  
+  Serial.print("Digital: ");
+  Serial.print(digitalRead(FLAME_SENSOR_DIGITAL_PIN));
+  Serial.print("\t Analog: ");
+  Serial.print(analogReadMilliVolts(FLAME_SENSOR_ANALOG_PIN));
+  Serial.println();
 }
